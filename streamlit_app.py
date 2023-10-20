6d897abaf1e8c18d73fdc188c9a039f66cf4be2f
DomenicaValle <148534821+DomenicaValle@users.noreply.github.com>
2023-10-20 10:49:09 +0200

    Update streamlit_app.py
    Changed breakfast header
    Added emojis to each item + added avo toast
    Added backslash apostrophe to Parent's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit 
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rcoket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Parent\'s New Healthy Diner')
+  
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rcoket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
