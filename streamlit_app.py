7f77b56aa0e71a9040602a37fb972140d33a6809
DomenicaValle <148534821+DomenicaValle@users.noreply.github.com>
2023-10-24 10:05:32 +0200

    Update streamlit_app.py
    Adding fruityvice api to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table
 streamlit.dataframe(fruits_to_show)
+
+# New section to display frutivice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruitvice_response)
